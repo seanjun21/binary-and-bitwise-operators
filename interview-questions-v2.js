d48ebc53201f906a5ad0eede6a6198b472c3471d
Eric Snell <eric_snell21@yahoo.com>
2016-08-17 17:14:07 -0400

    Logical Operator interview questions complete

--- interview-questions-v2.js
+++ interview-questions-v2.js
@@ -1,12 +1,31 @@
 // Write a function which sets the third bit of a number.
-
-4: 0 1 0 0
-
+var setThird = function(int) {
+	return (int | 8);
+};
 
 
 // Write a function which toggles the third bit of a number.
-// Write a function which clears (sets to zero) the third bit of a number.
-// Write a function which tells you whether the third bit of a number is set.
+var toggleThird = function(int) {
+	return (int ^ 8);
+};
 
-// 15: 1 1 1 1
-// 34: 1 0 0 0 1 0
+
+
+// Write a function which clears (sets to zero) the third bit of a number.
+var clearThird = function(int) {
+	return ((int | 8) ^ 8);
+// return (~8 & int)
+};
+
+
+// Write a function which tells you whether the third bit of a number is set.
+var thirdIsSet = function(int) {
+	if ((int & 8) === 8) {
+		alert('Sean is the man, and the third is set');
+		console.log('Sean is the man, and the third is set');
+		return true;
+	} else {
+		alert('Sean is still the man, even though third isn\'t set');
+		return false;
+	}
+};
